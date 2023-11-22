d2e39481078dbf15667c49c96e6c9eed1ee3309b
Prateek_Padhy <prateek_padhy@swissre.com>
2023-11-22 00:02:58 +0000

    fix: add rotterdom layer

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ const map = new mapboxgl.Map({
   center: [4.47917, 51.9225],
   zoom: 9,
 });
+
+map.on("load", () => {
+  map.addSource("source:rotterdam", {
+    type: "geojson",
+    data: "rotterdam-geojson.json",
+  });
+
+  map.addLayer({
+    id: "layer:rotterdam",
+    type: "fill",
+    source: "source:rotterdam",
+    layout: {},
+    paint: {
+      "fill-color": "#0080ff",
+      "fill-opacity": 0.5,
+    },
+  });
+
+  map.addLayer({
+    id: "outline:rotterdam",
+    type: "line",
+    source: "source:rotterdam",
+    layout: {},
+    paint: {
+      "line-color": "#000",
+      "line-width": 0.25,
+    },
+  });
+});
