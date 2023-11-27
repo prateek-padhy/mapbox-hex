f9e8f44a0ce918cb78451733759c26d2001852d4
Prateek_Padhy <prateek_padhy@swissre.com>
2023-11-27 09:52:09 +0000

    add h3 and convert geojson to hexes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { use, useEffect, useMemo, useRef, useState } from "react";
+import { featureToH3Set } from "geojson2h3";
+import { FeatureCollection, GeoJsonProperties, Geometry } from "geojson";
+import { cellToBoundary } from "h3-js";
 import mapboxgl from "mapbox-gl";
 import rotterdamGeojson from "../static/rotterdam-geojson.json";
 
@@ -14,6 +17,24 @@ export default function Home() {
   const [lat, setLat] = useState(51.9225);
   const [zoom, setZoom] = useState(8);
 
+  const [currentHexResolution, setCurrentHexResolution] = useState<number>(0);
+  const [locationSourceGeoJson, setLocationSourceGeoJson] =
+    useState<FeatureCollection<Geometry, GeoJsonProperties>>();
+
+  const getGeoJsonForHexes = (
+    locationHexes: string[]
+  ): FeatureCollection<Geometry, GeoJsonProperties> => ({
+    type: "FeatureCollection",
+    features: locationHexes.map((locationHex) => ({
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Polygon",
+        coordinates: [cellToBoundary(locationHex, true)],
+      },
+    })),
+  });
+
   useEffect(() => {
     if (map.current || !mapContainer.current) return;
 
@@ -23,13 +44,36 @@ export default function Home() {
       center: [lng, lat],
       zoom: zoom,
     });
+  });
 
+  useEffect(() => {
+    let resolution = 7;
+
+    if (zoom > 11 && zoom <= 13) resolution = 8;
+    if (zoom > 13) resolution = 9;
+
+    if (resolution <= currentHexResolution) return;
+
+    setCurrentHexResolution(resolution);
+  }, [zoom, currentHexResolution]);
+
+  useEffect(() => {
     if (!map.current) return;
+
+    const locationHexes: string[] = featureToH3Set(
+      rotterdamGeojson as FeatureCollection<Geometry, GeoJsonProperties>,
+      currentHexResolution
+    );
+    setLocationSourceGeoJson(() => getGeoJsonForHexes(locationHexes));
+  }, [map, currentHexResolution]);
+
+  useEffect(() => {
+    if (!map.current || !locationSourceGeoJson) return;
 
     map.current.on("load", () => {
       map.current.addSource("source:rotterdam", {
         type: "geojson",
-        data: rotterdamGeojson,
+        data: locationSourceGeoJson,
       });
 
       map.current.addLayer({
@@ -60,7 +104,8 @@ export default function Home() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-  });
+  }),
+    [locationSourceGeoJson];
 
   return (
     <main id="map">
